e5c4c00a50dd9e29b01ce054254d8a72be512056
𝑫𝒓𝒂𝒌𝒐𝑿𝑵𝒂𝒆𝒆𝒎 <nansari7287@gmail.com>
2026-09-14 15:37:03 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
+// Project-level build.gradle.kts
 plugins {
     id("com.android.application") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false
-    
-    // 🚨 MASTER FIX: 15.0.0 सर्वर पर नहीं है, इसलिए स्टेबल 14.0.2 यूज़ कर रहे हैं।
-    id("com.chaquo.python") version "14.0.2" apply false
+    id("com.chaquo.python") version "15.0.0" apply false
 }
